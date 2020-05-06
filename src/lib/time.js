3eee4988ab0a2785a9bb24d6e317401fe1864bcb
Maczi01 <mateusz.w.twardy@gmail.com>
2020-05-06 20:19:43 +0200

    add some tests for training

--- src/lib/time.js
+++ src/lib/time.js
@@ -1,7 +1,23 @@
-const getTimeInMinutesAndSeconds = (totalTimeInSeconds) =>{
+const getTimeInMinutesAndSeconds = (totalTimeInSeconds) => {
     const minutesLeft = Math.floor(totalTimeInSeconds / 60);
     const secondsLeft = Math.floor(totalTimeInSeconds % 60);
     return [minutesLeft, secondsLeft]
 }
 
-export { getTimeInMinutesAndSeconds}
+function fizzBuzz(i) {
+    let result = '';
+    // let out = '';
+    if (i % 3 === 0) {
+        result += 'Fizz';
+    }
+    if (i % 5 === 0) {
+        result += 'Buzz';
+    }
+    return result || i;
+}
+
+function xxx(str) {
+    return str.split("").reduce((xyz, abc)=> abc + xyz, '');
+}
+
+export {getTimeInMinutesAndSeconds, fizzBuzz, xxx}
