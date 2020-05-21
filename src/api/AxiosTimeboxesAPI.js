99ee67c9ce8dfe25a562574b0ec86d3522f95791
Maczi01 <mateusz.w.twardy@gmail.com>
2020-05-21 13:48:24 +0200

    change url to backend

---
+++ src/api/AxiosTimeboxesAPI.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+// const BASE_URL = "http://localhost:3010/timeboxes";
+const BASE_URL = "http://localhost:8080/api/";
+
+const AxiosTimeboxesApi = {
+    getAllTimeboxes: async () => {
+        const response = await axios.get(BASE_URL);
+        const timeboxes = response.data;
+        return timeboxes;
+    },
+
+    addNewTimebox: async (timeboxToAdd) => {
+        const response = await axios.post(BASE_URL, timeboxToAdd);
+        const addedTimebox = response.data
+        return addedTimebox;
+    },
+
+    replaceTimebox: async (timeboxToReplace) => {
+        if (!timeboxToReplace.id) {
+            throw new Error("This timebox haven't id!")
+        }
+        const response = await axios.put(`${BASE_URL}/${timeboxToReplace.id}`, timeboxToReplace);
+        const editedTimebox = response.data
+        return editedTimebox;
+    },
+
+    removeTimebox: async (timeboxToRemove) => {
+        if (!timeboxToRemove.id) {
+            throw new Error("This timebox haven't id!")
+        }
+        await axios.delete(`${BASE_URL}/${timeboxToRemove.id}`);
+
+    }
+};
+
+
+export default AxiosTimeboxesApi;
--- src/components/TimeboxList.js
+++ src/components/TimeboxList.js
@@ -2,74 +2,7 @@ import React from 'react'
 import EditableTimebox from "./EditableTimebox";
 import TimeBoxCreator from "./TimeBoxCreator";
 import ErrorCatcher from "./ErrorCatcher";
-import * as uuid from "uuid";
-
-const timeboxes = [{
-    id: "dxgcbhjk",
-    title: "Zmiana koloru w css",
-    totalTimeInMinutes: "10",
-    isEditing: false
-}, {
-    id: "dxgcbhsadasdasjk",
-    title: "Wyśrodkowanie diva",
-    totalTimeInMinutes: "40",
-    isEditing: false
-}, {
-    id: "dxgffffffcbhjk",
-    title: "Wstawianie średników na końcu",
-    totalTimeInMinutes: "20",
-    isEditing: false
-}]
-
-const findIndexById = (id) => {
-    const result = timeboxes.findIndex(timebox => timebox.id == id);
-    if (result < 0) {
-        throw new Error("Coś nie tak...")
-    }
-    return result;
-}
-
-const TimeboxesApi = {
-    getAllTimeboxes: async () => {
-        await wait(1000);
-        console.log("get", timeboxes)
-        return [...timeboxes]
-    },
-
-    addNewTimebox: async (timeboxToAdd) => {
-        await wait(1000);
-        const addedTimebox = {...timeboxToAdd, id: uuid.v4()}
-        timeboxes.push(addedTimebox);
-        console.log('post', timeboxes)
-        return addedTimebox;
-    },
-
-    replaceTimebox: async (timeboxToReplace) => {
-        await wait(1000);
-        if (!timeboxToReplace.id) {
-            throw new Error("Nie mozna zastąpić elementu bez id")
-        }
-        const index = findIndexById(timeboxToReplace.id);
-        const replacedTimebox = {...timeboxToReplace};
-        timeboxes[index] = replacedTimebox;
-        console.log('put', timeboxes)
-        return replacedTimebox
-    },
-    removeTimebox: async (timeboxToRemove) => {
-        await wait(1000);
-        if (!timeboxToRemove.id) {
-            throw new Error("Nie mozna zastąpić elementu bez id")
-        }
-        const index = findIndexById(timeboxToRemove.id);
-        timeboxes.splice(index, 1);
-        console.log('delete', timeboxes)
-    }
-}
-const wait = (ms = 1000) => {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms)
-    })
-};
+import TimeboxesApi from '../api/AxiosTimeboxesAPI'
 
 
 class TimeboxList extends React.Component {
@@ -146,9 +79,7 @@ class TimeboxList extends React.Component {
         this.addTimebox(createdTimebox);
     }
 
-
     render() {
-        // const {timeboxes} = this.state;
         return (
             <>
                 <TimeBoxCreator
@@ -160,7 +91,6 @@ class TimeboxList extends React.Component {
                     {this.state.timeboxes.map((timebox, index) =>
                         <EditableTimebox key={timebox.id}
                                          id={timebox.id}
-
                                          timebox={timebox}
                                          index={index}
                                          title={timebox.title}
