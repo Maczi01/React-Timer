73b86ac519708ac4b33a7c4bb806dab845b43739
Maczi01 <mateusz.w.twardy@gmail.com>
2020-06-02 17:39:03 +0200

    remove unused code

--- src/components/EditableTimebox.js
+++ src/components/EditableTimebox.js
@@ -2,43 +2,6 @@ import React from 'react';
 import Timebox from "./Timebox";
 import TimeboxEditForm from "./TimeboxEditForm";
 
-// class EditableTimebox extends React.Component {
-//     state = {
-//         title: this.props.title,
-//         totalTimeInMinutes: this.props.totalTimeInMinutes,
-//         isEditing: false,
-//     }
-//     // changeEdit = () => {
-//     //     this.setState({
-//     //         isEditing: !this.state.isEditing
-//     //     })
-//     // }
-//     handleSubmit = (e) => {
-//         e.preventDefault();
-//         this.props.onEdit(this.props.index, {
-//             ...this.props.timebox,
-//             title: this.state.title,
-//             totalTimeInMinutes: this.state.totalTimeInMinutes
-//         });
-//         this.setState({title: "", totalTimeInMinutes: ""})
-//         // this.changeEdit()
-//     }
-//
-//
-//     handleTitleChange = e => {
-//         this.setState({title: e.target.value});
-//     };
-//     handleTotalTimeInMinutesChange = e => {
-//         this.setState({totalTimeInMinutes: e.target.value});
-//     };
-
-// const {isEditing} = this.state;
-
-// if (totalTimeInMinutes <= 0) {
-//     throw new Error("Niedobry czas!!!!")
-// }
-
-
 const EditableTimebox = ({title, totalTimeInMinutes, id, onDelete, updateTimebox, changeEdit, isEditing}) => {
     return (
         <div className="Timebox">
