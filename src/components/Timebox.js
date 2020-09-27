885e7b2fe5793bbf80d9f6559b4644066703e763
Maczi01 <mateusz.w.twardy@gmail.com>
2020-09-27 22:35:14 +0200

    changes in description

--- src/components/Timebox.js
+++ src/components/Timebox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Timebox = ({title, totalTimeInMinutes}) => (
     <div>
-        <h3>{title} - {totalTimeInMinutes} minutes</h3>
+        <h3>{title} - {totalTimeInMinutes} min</h3>
     </div>
 )
 
